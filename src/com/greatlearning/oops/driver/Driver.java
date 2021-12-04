d52292bf93b4de05cf76a7f332fef6b5aa45054c
Atish Ghosh <atishk2012@gmail.com>
2021-12-04 16:07:30 +0530

    First commit

---
+++ src/com/greatlearning/oops/driver/Driver.java
@@ -0,0 +1,34 @@
+package com.greatlearning.oops.driver;
+
+import com.greatlearning.oops.admindep.AdminDepartment;
+import com.greatlearning.oops.hrdep.HrDepartment;
+import com.greatlearning.oops.techdep.TechDepartment;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		AdminDepartment admin = new AdminDepartment();
+		System.out.println(admin.departmentName());
+		System.out.println(admin.getTodaysWork());
+		System.out.println(admin.getWorkDeadline());
+		System.out.println(admin.isTodayAHoliday());
+		System.out.println();
+
+		HrDepartment hr = new HrDepartment();
+		System.out.println(hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday());
+		System.out.println();
+
+		TechDepartment tech = new TechDepartment();
+		System.out.println(tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayAHoliday());
+
+	}
+
+}
---
+++ src/com/greatlearning/oops/hrdep/HrDepartment.java
@@ -0,0 +1,22 @@
+package com.greatlearning.oops.hrdep;
+
+import com.greatlearning.oops.superdep.SuperDepartment;
+
+public class HrDepartment extends SuperDepartment {
+	public String departmentName() {
+		return "Welcome to Hr Department";
+	}
+
+	public String doActivity() {
+		return "team Lunch";
+	}
+
+	public String getTodaysWork() {
+		return "Fill today’s timesheet and mark your attendance";
+	}
+
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+}
